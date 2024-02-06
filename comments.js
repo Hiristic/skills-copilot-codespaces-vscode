343c5cedce41bfbb58911ebad9e6bbb890cc2c7d
Hiristic <55379302+Hiristic@users.noreply.github.com>
2024-02-06 13:24:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,26 @@
 // create web server
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// load the comments
-const comments = require('./comments');
-
-// serve the comments
-app.get('/comments', (req, res) => {
-    res.json(comments);
+// create a server
+const server = require('http').createServer(app);
+// create websocket server
+const io = require('socket.io')(server);
+// create a web server
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
-
-app.listen(port, () => {
-    console.log('Server listening at http://localhost:' + port);
+// create a websocket server
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
+// start the server
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+}); 
